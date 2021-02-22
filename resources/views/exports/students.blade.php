851b1ad8ee75fd01ecf87d3f514b20036f238054
ktmsulaim <ktmsulaim@ktmsulaims-iMac.local>
2021-02-22 22:25:58 +0530

    Added support for multi entry (Jobs)

--- resources/views/exports/students.blade.php
+++ resources/views/exports/students.blade.php
@@ -25,24 +25,64 @@
     <tbody>
         @if (count($students) > 0)
             @foreach ($students as $key => $student)
+            {{-- 
+                If student has data which belong to repeatable module
+                Else    
+            --}}
+                @if ($student->itemGroups && count($student->itemGroups) > 1)
+                    @foreach ($student->itemGroups as $itemGroupKey => $itemGroup)
+                    <tr>
+                        @if ($itemGroupKey == 0)
+                            <td>{{ $key + 1 }}</td>
+                        @else
+                            <td></td>
+                        @endif
+                        @foreach ($modules as $module3)
+                            @if ($module3->itemUsers()->exists())
+                                @if ($module3->isRepeatable())
+                                    @if ($itemGroup->module_id == $module3->id && $itemGroup->itemUsers)
+                                        @foreach ($itemGroup->orderedItemUsers() as $itemUser)
+                                            <td>{{ $itemUser->getValue() }}</td>
+                                        @endforeach
+                                    @else
+                                        <td colspan="{{ count($module3->items) }}"></td>
+                                    @endif
+                                @elseif(!$module->isRepeatable())
+                                    @foreach ($module3->orderedItemUsers($student->id) as $itemUser)
+                                        @if ($itemGroupKey == 0)
+                                            <td>{{ $itemUser->getValue() }}</td>
+                                        @else
+                                            <td></td>
+                                        @endif
+                                    @endforeach
+                                @else
+                                    <td></td>
+                                @endif
+                            @else
+                                <td colspan="{{ count($module3->items) }}"></td>
+                            @endif
+                        @endforeach
+                    </tr>
+                    @endforeach
+                @else
                 <tr>
-                    <td>{{ $key + 1 }}</td>
-                    @foreach ($modules as $module3)
-                        @if ($module3->itemUsers()->exists())
-                            @if ($module3->isRepeatable())
+                    <td>{{ $key + 1  }}</td>
+                    @foreach ($modules as $module4)
+                        @if ($module4->itemUsers()->exists())
+                            @if ($module4->isRepeatable())
                                 @php
                                 // get latest group
-                                $itemGroup = $module3->itemGroups()->where('user_id', $student->id)->latest()->first();
+                                $itemGroup = $module4->itemGroups()->where('user_id', $student->id)->latest()->first();
                                 @endphp
                                 @if ($itemGroup && $itemGroup->itemUsers)
                                     @foreach ($itemGroup->orderedItemUsers() as $itemUser)
                                         <td>{{ $itemUser->getValue() }}</td>
                                     @endforeach
                                 @else
-                                    <td colspan="{{ count($module3->items) }}"></td>
+                                    <td colspan="{{ count($module4->items) }}"></td>
                                 @endif
                             @else
-                                @foreach ($module3->orderedItemUsers($student->id) as $itemUser)
+                                @foreach ($module4->orderedItemUsers($student->id) as $itemUser)
                                     <td>{{ $itemUser->getValue() }}</td>
                                 @endforeach
                             @endif
@@ -51,6 +91,7 @@
                         @endif
                     @endforeach
                 </tr>
+                @endif
             @endforeach
         @endif
     </tbody>
